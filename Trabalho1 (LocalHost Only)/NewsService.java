5e1256f16eb20feb5def9a2d1ec5c12cee1dfa9f
Gustavo Freitas <87988054+GustavoHFreitas@users.noreply.github.com>
2024-11-19 02:29:07 -0300

    Delete Trabalho1 (LocalHost Only) directory

--- Trabalho1 (LocalHost Only)/NewsService.java
+++
@@ -1,9 +0,0 @@
-import java.rmi.Remote;
-import java.rmi.RemoteException;
-
-public interface NewsService extends Remote {
-    void subscribe(Subscriber subscriber) throws RemoteException;
-    void unsubscribe(Subscriber subscriber) throws RemoteException;
-    void publishNews(String news) throws RemoteException;
-    int getNextSubscriberNumber() throws RemoteException;
-}
--- Trabalho1 (LocalHost Only)/Subscriber.java
+++
@@ -1,7 +0,0 @@
-import java.rmi.Remote;
-import java.rmi.RemoteException;
-
-public interface Subscriber extends Remote {
-    void receiveNews(String news) throws RemoteException;
-    String getName() throws RemoteException;
-}
